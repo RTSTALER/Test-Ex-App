323e74b186398a152aa7e5b9b2757d51ec65f4a8
RTSTALER <37743421+RTSTALER@users.noreply.github.com>
2019-10-22 19:09:38 +0900

    Update 1.2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace TestExApp
 {
     class Program
     {
         static int MaxBigKey = 0;
+        static List<char> constituentsChar = new List<char>();
         public static void Main(string[] args)
         {
             Stopwatch stopWatch = new Stopwatch();
@@ -25,16 +23,8 @@ namespace TestExApp
             SearchWatch.Stop();
             Console.WriteLine("Complete! Check result string[] on Debug mode ;)");
             stopWatch.Stop();
-            TimeSpan ts = stopWatch.Elapsed;
-            TimeSpan ts2 = SearchWatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts2.Hours, ts2.Minutes, ts2.Seconds,
-                ts2.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
-            Console.WriteLine("TranslateTime " + elapsedTime2);
+            Console.WriteLine("RunTime " + stopWatch.ElapsedMilliseconds + " ms");
+            Console.WriteLine("TranslateTime " + SearchWatch.ElapsedMilliseconds + " ms");
             Console.ReadKey();
         }
 
@@ -43,30 +33,39 @@ namespace TestExApp
             string[] result = new string[titles.Length];
             for (int i = 0; i < titles.Length; i++)
             {
-                string words = "";
                 string key = "";
-                string title = titles[i];
-                key = title.Substring(0, MaxBigKey);
-                for (int c = 0; c < titles[i].Length; c++)
-                {
-                    var s = tree.GetByPrefix(key).Select(e => e.Value).ToArray();
-                    if (s.Length == 0)
-                        key = key.Remove(key.Length - 1);
-                    else
-                    {
-                        words += s[0];
-                        c = 0;
-                        title = title.Substring(key.Length);
-                        if (key == "" || title == "")
-                            break;
-                        if (title.Length >= MaxBigKey)
-                            key = title.Substring(0, MaxBigKey);
-                        else
-                            key = title;
+                var words = "";
+                var tempTitile = titles[i];
+                var unknownSymbols = "" ;
+                
+                foreach (var c in tempTitile)
+                    if (!constituentsChar.Contains(c))
+                        unknownSymbols += c;
+                foreach (var s in unknownSymbols)
+                   tempTitile = tempTitile.Replace(s.ToString(), s+" ");
 
+                var lines = tempTitile.Split(' ');
+                foreach(var line in lines)
+                {
+                    int offset = 0;
+                    for (int c = 0; c < line.Length; c++)
+                    {
+                        key += line[c];
+                        var s = tree.GetByPrefix(key).Select(e => e.Value).ToArray();
+                        if (s.Length != 1)
+                            continue;
+                        else
+                        {
+                            offset += key.Length;
+                            words += s[0];
+                            key = "";
+                        }
+                            
                     }
+                    if (offset != 0)
+                        words += line.Substring(0, offset);
                 }
-                result[i] = words;
+                result[i] += words;
             }
             return result;
         }
@@ -100,6 +99,9 @@ namespace TestExApp
                     string[] lines = reader.ReadLine().Split(';');
                     if (lines[0].Length > MaxBigKey)
                         MaxBigKey = lines[0].Length;
+                    foreach (var c in lines[0])
+                        if (!constituentsChar.Contains(c))
+                            constituentsChar.Add(c);
                     resultDictonary.Add(lines[0], lines[1]);
                 }
                 return resultDictonary;
